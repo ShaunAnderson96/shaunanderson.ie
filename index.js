df6ad36a7f965cb8a5f4f9e2710b801154207419
Shaun Anderson <admin@PR2404-10.lyitnet.lyit.ie>
2018-05-11 23:08:40 +0100

    updated 11/05

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 window.onload = function(){
 typeWrite();
-  start();
+start();
 
   $(function() {
     $('body').removeClass('fade-out');
   });
+
+  //once loaded, delay the move to the who am i section (jquery)
+  $("html, body").delay(3200).animate({
+    scrollTop: $('#target').offset().top 
+}, 2000);
+  
+}
+//when page is reloaded again it is moved to the top of the page
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
 }
 //typewrite my name
 function typeWrite(){
 var typed = new Typed("#typed", {
-    strings: ["SHAUN ^100 ANDERSON."],
-    typeSpeed: 150,
+    strings: ["SHAUN ^100 ANDERSON"],
+    typeSpeed: 100,
     showCursor: false,
     startDelay: 400,
   });
@@ -21,8 +31,8 @@ var typed = new Typed("#typed2", {
 //   smartBackspace: true,
 //   backDelay:2500,
   //loop: true,
-  typeSpeed: 100,
-  startDelay: 7500,
+  typeSpeed: 120,
+  startDelay: 5500,
   showCursor: true,
   cursorChar: '|',
 });
